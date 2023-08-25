96ea20c2427b7ab9f5c331837a88952f55610dea
moaliyou <moaliyou47@gmail.com>
2023-08-25 18:27:59 +0300

    Solved: internet profile problem assigment in Basic android kotlin compose course

---
+++ src/main/kotlin/practice/InternetProfile.kt
@@ -0,0 +1,33 @@
+package practice
+
+fun main() {
+    val amanda = Person("Amanda", 33, "play tennis", null)
+    val atiqah = Person("Atiqah", 28, "climb", amanda)
+
+    amanda.showProfile()
+    atiqah.showProfile()
+}
+
+
+class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
+    fun showProfile() {
+        if (hasReferrer()) {
+            println("Name : ${this.name}")
+            println("Age : ${this.age}")
+            println("Likes to ${this.hobby}. " +
+                    "Has a referrer named ${this.referrer?.name}, " +
+                    "who likes to ${this.referrer?.hobby}."
+            )
+        } else {
+            println("Name : ${this.name}")
+            println("Age : ${this.age}")
+            println("Likes to ${this.hobby}. Doesn't have a referrer."
+            )
+        }
+    }
+
+    private fun hasReferrer(): Boolean {
+        return referrer != null
+    }
+
+}
